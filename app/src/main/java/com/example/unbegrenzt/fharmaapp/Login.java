c8a968b0e7814f11c529cfd5f7c874570cd1d3e0
Unbegrenzt <jmoralescenteno@gmail.com>
2017-05-02 00:58:05 -0600

    mas diseño

---
+++ app/src/main/java/com/example/unbegrenzt/fharmaapp/Login.java
@@ -0,0 +1,84 @@
+package com.example.unbegrenzt.fharmaapp;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.widget.ImageView;
+
+public class Login extends FragmentActivity{
+
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally
+     * to access previous and next pages.
+     */
+    ViewPager pager = null;
+
+    /**
+     * The pager adapter, which provides the pages to the view pager widget.
+     */
+    //ViewPagerAdapter pagerAdapter;
+    ImageView dot1,dot2,dot3;
+
+
+
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        this.setContentView(R.layout.login);
+
+        dot1 = (ImageView)this.findViewById(R.id.dot1);
+        dot2 = (ImageView)this.findViewById(R.id.dot2);
+        dot3 = (ImageView)this.findViewById(R.id.dot3);
+
+        // Instantiate a ViewPager
+        this.pager = (ViewPager) this.findViewById(R.id.pager);
+
+        // Create an adapter with the fragments we show on the ViewPager
+        ViewPagerAdapter adapter = new ViewPagerAdapter(
+                getSupportFragmentManager());
+        adapter.addFragment(PagesAdapter.newInstance(getResources()
+                .getColor(R.color.atenuante), 0));
+        adapter.addFragment(PagesAdapter.newInstance(getResources()
+                .getColor(R.color.color_acentuado), 1));
+        adapter.addFragment(PagesAdapter.newInstance(getResources()
+                .getColor(R.color.color_base), 4));
+        this.pager.setAdapter(adapter);
+        getItem();
+    }
+
+    //TODO:encontrar la forma de mover los circulos con las imagenes
+    public void getItem(){
+        // se trata e cambiar los circulos con las imagenes
+        if (this.pager.getCurrentItem() == 0) {
+
+            dot1.setBackgroundResource(R.drawable.selecteditem_dot);
+            dot2.setBackgroundResource(R.drawable.nonselecteditem_dot);
+            dot3.setBackgroundResource(R.drawable.nonselecteditem_dot);
+
+        } else if (this.pager.getCurrentItem() == 1) {
+
+            dot1.setBackgroundResource(R.drawable.nonselecteditem_dot);
+            dot2.setBackgroundResource(R.drawable.selecteditem_dot);
+            dot3.setBackgroundResource(R.drawable.nonselecteditem_dot);
+
+        } else if (this.pager.getCurrentItem() == 2) {
+
+            dot1.setBackgroundResource(R.drawable.nonselecteditem_dot);
+            dot2.setBackgroundResource(R.drawable.nonselecteditem_dot);
+            dot3.setBackgroundResource(R.drawable.selecteditem_dot);
+
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        // Return to previous page when we press back button
+        if (this.pager.getCurrentItem() == 0)
+            super.onBackPressed();
+        else
+            this.pager.setCurrentItem(this.pager.getCurrentItem() - 1);
+
+    }
+}
+
---
+++ app/src/main/java/com/example/unbegrenzt/fharmaapp/ViewPagerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.unbegrenzt.fharmaapp;
+
+/*
+  Created by Unbegrenzt on 1/5/2017.
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ *
+ * @author amatellanes
+ *
+ */
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    // List of fragments which are going to set in the view pager widget
+    List<Fragment> fragments;
+
+    /**
+     * Constructor
+     *
+     * @param fm
+     *            interface for interacting with Fragment objects inside of an
+     *            Activity
+     */
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+        this.fragments = new ArrayList<Fragment>();
+    }
+
+    /**
+     * Add a new fragment in the list.
+     *
+     * @param fragment
+     *            a new fragment
+     */
+    public void addFragment(Fragment fragment) {
+        this.fragments.add(fragment);
+    }
+
+    @Override
+    public Fragment getItem(int arg0) {
+        return this.fragments.get(arg0);
+    }
+
+    @Override
+    public int getCount() {
+        return this.fragments.size();
+    }
+
+}
+
